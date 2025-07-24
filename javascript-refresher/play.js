90d391e1a898ac4bb9866b5421d53fef2dafb443
Muaaz Shoaib <muaazshoaib1670@gmail.com>
2025-07-24 13:29:06 +0500

    Understanding Spread & Rest Operators

--- javascript-refresher/play.js
+++ javascript-refresher/play.js
@@ -8,6 +8,19 @@ const person = {
 
 const hobbies = ["Sports", "Cooking"];
 
-hobbies.push("Programming");
+// const copiedArray = hobbies.slice();
+// const copiedArray = [hobbies];
+const copiedArray = [...hobbies];
 
-console.log(hobbies);
+console.log(copiedArray);
+
+// const toArray = (arg1, arg2, arg3) => {
+//   return [arg1, arg2, arg3];
+// };
+
+const toArray = (...args) => {
+  return args;
+};
+
+console.log(toArray(1, 2, 3));
+console.log(toArray(1, 2, 3, 4));
